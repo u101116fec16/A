0bc71fb312ce81fcde79827b4f8a3356cfc15f45
u101116fec16 <43662288+u101116fec16@users.noreply.github.com>
2019-04-11 16:08:07 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,13 @@
+
+public class A {
+	// A() {
+	// System.out.println("In A constructor");
+	// }
+	A(int i) {
+		System.out.println("In A constructor "+i);
+	}
+	A()
+	{
+		System.out.println("In A balnk argument");
+	}
+}
